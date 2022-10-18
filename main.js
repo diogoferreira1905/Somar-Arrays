d28e0de5f2644dff423f5980392b8f308c47c18d
diogoferreira1905 <113584514+diogoferreira1905@users.noreply.github.com>
2022-10-18 08:53:03 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+const nums = ["1", "2", "3","4"];
+
+ 
+ 
+function somarValores(){
+    var soma = 0;
+    
+     
+    for (var i=0; i<quant.length; i++){
+      
+               teste[i] = quant[i].value;      
+            soma += teste[i];
+     }  
+      document.getElementById("resultado").innerHTML = soma;
+    }
